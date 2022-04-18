a79c0a14e1c11321cd390b551ff8f0283a09c8d1
szqdidi <2167622337@qq.com>
2022-04-18 22:03:48 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+ 
+export default {
+  input: 'src/index.ts', // 打包入口
+  output: { // 打包出口
+	file: 'dist/index.ts',
+    format: 'umd', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
+  },
+  plugins: [ // 打包插件
+    resolve(), // 查找和打包node_modules中的第三方模块
+    commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+    typescript() // 解析TypeScript
+  ]
+};
